a007a0de64b2aaddac6e41c110275dafefa7e4c0
MaeganW <maeganwomble@gmail.com>
2018-11-09 17:02:37 -0600

    while loops

--- iteration.py
+++ iteration.py
@@ -18,3 +18,39 @@ for num in nums:
     sum = sum + num
 
 print(sum)
+
+print('===========')
+
+word = 'hello'
+
+for char in word:
+  print(char)
+
+print('===========')
+
+mytuples = [(1,1,3),(1,2,2),(1,3,1)]
+
+for item in mytuples:
+  print item
+
+for a,b,c in mytuples:
+  print b
+
+print('=========== iterates through keys fo doc by default')
+
+doc = {
+  'one': 1,
+  'two': 2,
+  'three': 3
+}
+
+for item in doc:
+  print item
+
+print('=========== but you can grab the items like so...')
+
+for item in doc.items():
+  print(item)
+
+for key,value in doc.items():
+  print(value)
---
+++ while.py
@@ -0,0 +1,5 @@
+x = 0
+
+while x < 10:
+  print('hello {x}'.format(x = x))
+  x = x + 1
