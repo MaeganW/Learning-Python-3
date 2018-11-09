55f55828cdcc573bff8aba3cb87473b463a660dd
MaeganW <maeganwomble@gmail.com>
2018-11-09 16:22:25 -0600

    comparison operators

---
+++ comparison.py
@@ -0,0 +1,9 @@
+a = ['ha', 'ha', 'ha']
+print(a);
+
+if len(a) < 2:
+  print('true')
+elif len(a) == 3 and a[0] == 'ha':
+  print('equal to 3')
+else:
+  print('false')
--- hello_world.py
+++ hello_world.py
@@ -7,3 +7,11 @@ print(9/3)
 print(8/3)
 print(6/3)
 print(7%3)
+
+a = 1
+print(a)
+a = ['ha', 'ha', 'ha']
+print(a);
+
+if len(a) > 2:
+  print('true')
